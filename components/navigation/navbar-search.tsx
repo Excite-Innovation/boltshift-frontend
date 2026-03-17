511f5948303b8ece70fc6cd764db082fdc72767b
denilany <denilany1@gmail.com>
2026-03-17 16:26:46 +0300

    Refactor code by consolidating import statements into a single line where appropriate to reduce verbosity.

--- components/navigation/navbar-search.tsx
+++ components/navigation/navbar-search.tsx
@@ -1,10 +1,5 @@
 import { Field } from "@/components/ui/field";
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-  InputGroupButton,
-} from "@/components/ui/input-group";
+import { InputGroup, InputGroupAddon, InputGroupInput, InputGroupButton } from "@/components/ui/input-group";
 import { Search } from "lucide-react";
 
 export function NavbarSearch() {
@@ -25,7 +20,10 @@ export function NavbarSearch() {
         </InputGroupAddon>
 
         <InputGroupAddon align="inline-end" className="  ">
-          <InputGroupButton variant="default" className="w-17.25 h-12 py-2 px-3 rounded-l-none rounded-r-lg -mr-1.25">
+          <InputGroupButton
+            variant="default"
+            className="w-17.25 h-12 py-2 px-3 rounded-l-none rounded-r-lg -mr-1.25"
+          >
             Search
           </InputGroupButton>
         </InputGroupAddon>
