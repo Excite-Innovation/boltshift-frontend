55afb8b74c9a70d61d2f330861dbd0c517cfe0f1
Paul Mbingu <paulxmbingu@gmail.com>
2026-04-03 05:58:10 +0300

    Updated manifest for apple maskable

--- app/manifest.ts
+++ app/manifest.ts
@@ -57,13 +57,13 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: "/icons/maskable/Variant=512-maskable.png",
                 sizes: "512x512",
                 type: "image/png",
-                purpose: "any",
+                purpose: "maskable",
             },
             {
                 src: "/icons/maskable/Variant=icon-192-maskable.png",
                 sizes: "192x192",
                 type: "image/png",
-                purpose: "any",
+                purpose: "maskable",
             },
 
             // Apple icons
