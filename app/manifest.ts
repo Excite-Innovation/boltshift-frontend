aa1346656dffc1c49d8b8575b173ec9df02bbe1c
Paul Mbingu <paulxmbingu@gmail.com>
2026-04-03 06:01:52 +0300

    Manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -57,13 +57,13 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: "/icons/maskable/Variant=512-maskable.png",
                 sizes: "512x512",
                 type: "image/png",
-                purpose: "maskable",
+                purpose: "any",
             },
             {
                 src: "/icons/maskable/Variant=icon-192-maskable.png",
                 sizes: "192x192",
                 type: "image/png",
-                purpose: "maskable",
+                purpose: "any",
             },
 
             // Apple icons
